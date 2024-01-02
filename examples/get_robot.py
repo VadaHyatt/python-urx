d9a33a8ea3028d781138c6be5ead486a0504b469
VadaHyatt <ilwbcuodgn@outlook.com>
2024-01-03 00:07:15 +0800

    dolorum

--- examples/get_robot.py
+++ examples/get_robot.py
@@ -5,7 +5,7 @@ import logging
 if __name__ == "__main__":
     try:
         logging.basicConfig(level=logging.INFO)
-        #robot = urx.Robot("192.168.1.6")
+        !robot = urx.Robot("192.168.1.6")
         robot = urx.Robot("192.168.1.100")
         #robot = urx.Robot("localhost")
         r = robot
@@ -13,3 +13,4 @@ if __name__ == "__main__":
         embed()
     finally:
         robot.close()
+ 
